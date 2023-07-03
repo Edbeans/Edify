73284bb090917609fa861fe61a71d4585e34d6c4
Edward Ying <edwardying98@gmail.com>
2023-07-03 15:41:15 -0700

    layout steup

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -37,8 +38,8 @@ const Header: React.FC<HeaderProps> = ({
                         <RxCaretRight className='text-white' size={35}/>
                     </button>
                 </div>
+                {/* will only be able to see div below when you collapse to mobile view */}
                 <div className='flex md:hidden gap-x-2 items center'>
-                    {/* will only be able to see this button when you collapse to mobile view */}
                     <button className='rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition'>
                         <HiHome className='text-black' size={20}/>
                     </button>
@@ -46,7 +47,23 @@ const Header: React.FC<HeaderProps> = ({
                         <BiSearch className='text-black' size={20}/>
                     </button>
                 </div>
+                <div className='flex justify-between items-center gap-x-4'>
+                    <>
+                        <div>
+                            {/* in the future it will open our sign up and login modals  */}
+                            <Button onClick={() => {}} className='bg-transparent text-neutral-300 font-medium'>
+                                Sign Up
+                            </Button>
+                        </div>
+                        <div>
+                            <Button onClick={() => {}} className='bg-white px-6 py-2'>
+                                Log In
+                            </Button>
+                        </div>
+                    </>
+                </div>
             </div>
+            {children}
         </div>
     );
 }
